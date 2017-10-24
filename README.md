2a399a1b30442af0c9b9079728d5b40e42f8e93c
James Slaughter <james.slaughter08@gmail.com>
2017-10-24 12:51:20 -0500

    First time commit of Ionic Simple App

---
+++ README.md
@@ -0,0 +1,8 @@
+## COMMENTS
+
+Simple Ionic Show Greeting App.
+
+App built via ionic cli and uses toast messages
+
+Uses basic principles of an Ionic app.
+
---
+++ src/pages/home/home.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { ToastController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  personName: string;
+
+  constructor(private toast: ToastController) { }
+
+  showGreeting(name: string): void {
+    //console.log(name);
+    this.toast.create({
+      message: `Hello ${name}`,
+      duration: 3000
+    }).present();
+  }
+
+}
